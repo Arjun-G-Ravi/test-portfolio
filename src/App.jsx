0f2b6386642f2aa661b381066e622752c7dac69b
Arjun G Ravi <96014619+Arjun-G-Ravi@users.noreply.github.com>
2024-08-03 14:03:40 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Arjun G Ravi",
+  title: "ML Enginner",
+  email: "",
+  gitHub: "https://github.com/Arjun-G-Ravi",
+  instagram: "",
+  linkedIn: "www.linkedin.com/in/arjun-g-ravi",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
